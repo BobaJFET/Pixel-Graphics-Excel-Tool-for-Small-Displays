4bdc4cfb5e1341a15de7e3d155513a0c0f36a4ef
David Colby <85833990+BobaJFET@users.noreply.github.com>
2022-03-18 14:28:14 -0400

    forgetting things

--- Pixel Excel Tool for Adafruit GFX.py
+++ Pixel Excel Tool for Adafruit GFX.py
@@ -1,4 +1,3 @@
-#a helpful article I used as reference for openpyxl: https://www.geeksforgeeks.org/reading-excel-file-using-python/
 #Adafruit GFX website: https://learn.adafruit.com/adafruit-gfx-graphics-library/overview
 
 import openpyxl
